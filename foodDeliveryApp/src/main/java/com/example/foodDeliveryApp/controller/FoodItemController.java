d572ded6cf637705c07e6f77b85e98ed6dd7235b
deepakkrgup <deepakkumargupta62055@gmail.com>
2026-04-04 13:00:57 +0530

    Added CrossOrigin and updated backend

--- foodDeliveryApp/src/main/java/com/example/foodDeliveryApp/controller/FoodItemController.java
+++ foodDeliveryApp/src/main/java/com/example/foodDeliveryApp/controller/FoodItemController.java
@@ -7,7 +7,7 @@ import com.example.foodDeliveryApp.service.FoodItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/fooditem")
 public class FoodItemController {
--- foodDeliveryApp/src/main/java/com/example/foodDeliveryApp/controller/UserController.java
+++ foodDeliveryApp/src/main/java/com/example/foodDeliveryApp/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
